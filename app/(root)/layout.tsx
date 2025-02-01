5ea03239c451c5cdc045972f409c3f698c96f4ea
Anirudh Panwar <anirudh0976@gmail.com>
2025-02-02 00:03:58 +0530

    setup prisma for database and next auth for authentication

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from "../../components/ui/shared/Footer";
-import Navbar from "../../components/ui/shared/Navbar";
+import Footer from "@/components/ui/shared/Footer";
+import Navbar from "@/components/ui/shared/Navbar";
 
 export default function RootLayout({
     children,
