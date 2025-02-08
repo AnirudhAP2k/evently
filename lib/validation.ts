95954325169b5e81572c313d38fa8c01cc322ada
Anirudh Panwar <anirudh0976@gmail.com>
2025-02-09 01:44:07 +0530

    Added zod validation to the user credentials

---
+++ lib/validation.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod'
+
+export const LoginSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(1, {
+        message: 'Password is required'
+    }),
+})
+
+export const RegisterSchema = z.object({
+    email: z.string().email(),
+    password: z.string()
+        .min(8, { message: 'Minimum 8 characters are required' })
+        .regex(/[A-Z]/, { message: "At least one uppercase letter is required" })
+        .regex(/[a-z]/, { message: "At least one lowercase letter is required" })
+        .regex(/[0-9]/, { message: "At least one digit is required" })
+        .regex(/[!@#$%^&*]/, { message: "At least one special character is required" }),
+    name: z.string().min(1, { message: "Name is required" }),
+    confirmPassword: z.string()
+}).refine(({ password, confirmPassword }) => confirmPassword === password, {
+    message: "The passwords did not match",
+    path: ["confirmPassword"],
+});
