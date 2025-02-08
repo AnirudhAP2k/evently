f3996d56ea509aa1e8cc157c3771666978764fe4
Anirudh Panwar <anirudh0976@gmail.com>
2025-02-09 01:38:27 +0530

    Modified authentication file for Credentials Provider

--- auth.ts
+++ auth.ts
@@ -2,9 +2,8 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { prisma } from "@/db/prisma";
+import { prisma } from "@/lib/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { compare } from "bcryptjs";
 
 
 export const { handlers, auth, signIn, signOut }  = NextAuth({
@@ -19,32 +18,7 @@ export const { handlers, auth, signIn, signOut }  = NextAuth({
             clientSecret: process.env.GITHUB_SECRET || "",
         }),
         CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email: { label: "Email", type: "text" },
-                password: { label: "Password", type: "text" },
-            },
-            async authorize(credentials) {
-                if (!credentials?.email || !credentials?.password) {
-                    throw new Error("Credentials not provided");
-                }
-
-                const user = await prisma.user.findUnique({
-                    where: { email: credentials.email as string },
-                    select: { id: true, email: true, name: true, emailVerified: true, image: true, createdAt: true, updatedAt: true, password: true },
-                });
-                
-                if (!user) {
-                    throw new Error("User not found");
-                }
-
-                const isValid = user.password && await compare(credentials.password as string, user.password as string);
-                if (!isValid) {
-                    throw new Error("Invalid password");
-                }
-
-                return user;
-            },
+            name: "Credentials"
         }),
     ],
     session: {
