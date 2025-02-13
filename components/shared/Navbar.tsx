b1c9ed2c569c9c1f4cb245b36b8b90308c910e9d
Anirudh Panwar <anirudh0976@gmail.com>
2025-02-14 00:38:22 +0530

    Updates in the Navbar + Added OAuth authentication setup

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -16,27 +16,31 @@ const Navbar = async () => {
         <Link href='/' className='w-36'>
             <Image src='/assets/images/logo.svg' width={128} height={128} alt='logo' />
         </Link>
-        { !session && !session?.user && (
+        { session && session?.user && (
           <nav className='hidden md:flex-between w-full max-w-xs gap-5'>
             <NavItems />
           </nav>
         )}
         <div className='flex justify-end'>
           <div className='flex items-center gap-5'>
-            { !session && !session?.user ? (
+            { session && session?.user ? (
                 <>
-                  <form action={async () => {
+                  <form
+                    // className="hidden md:block"
+                     action={async () => {
                         "use server";
                         await signOut( { redirectTo: "/" })
                     }}>
-                    <Button asChild className="rounded-full" size="lg" type="submit">
-                      <a>Logout</a>
+                    <Button className="rounded-full" size="lg" type="submit">
+                      Logout
                     </Button>
                   </form>
-                  <Link href={`/user/${session?.id}`}>
+                  <Link href={`/user/${session?.user?.id}`}>
                     <Image
                       src={session?.user?.image}
                       alt={session?.user?.name}
+                      width={40}
+                      height={40}
                       className="rounded-full"
                       sizes="lg"
                     />
