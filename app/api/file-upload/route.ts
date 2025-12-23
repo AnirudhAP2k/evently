d1052f9487f6c726e76c88462414bf21e2ab6dc3
Anirudh Panwar <anirudh0976@gmail.com>
2025-12-23 18:24:46 +0530

    Minor code updates

--- app/api/file-upload/route.ts
+++ app/api/file-upload/route.ts
@@ -31,13 +31,13 @@ export async function POST(req: NextRequest) {
 
     const buffer = Buffer.from(await file.arrayBuffer());
 
-    const uploadResult: any = await new Promise((resolve, reject) => {
+    const uploadResult : any = await new Promise((resolve, reject) => {
       cloudinary.uploader.upload_stream(
         {
           folder: "uploads",
           resource_type: "image",
         },
-        (error, result) => {
+        (error : any, result : any) => {
           if (error) reject(error);
           resolve(result);
         }
