ef2978998b48ca8245ea4d67fa630fa8d6cdf6f5
Anirudh Panwar <anirudh0976@gmail.com>
2026-02-13 18:34:52 +0530

    Minor fixes in instrumentation

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,5 @@
 export async function register() {
-    if (process.env.NEXT_RUNTIME === "nodejs") {
+    if (process.env.NEXT_RUNTIME === "nodejs" && process.env.NODE_ENV !== "development") {
         const { initializeScheduler } = await import("@/lib/scheduler/cron-jobs");
         initializeScheduler();
     }
